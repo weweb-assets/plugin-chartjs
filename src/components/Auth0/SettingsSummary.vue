3f3c902a02b01b00c4cb730c0d5d8c04fa42d22f
Damien DURO <damien@weweb.io>
2021-10-04 18:37:20 +0200

    Fix colors

--- src/components/Auth0/SettingsSummary.vue
+++ src/components/Auth0/SettingsSummary.vue
@@ -103,10 +103,11 @@ export default {
         align-items: center;
     }
     &__url {
-        color: var(--ww-color-dark-500);
-        background-color: var(--ww-color-dark-200);
+        color: var(--ww-color-theme-dark-700);
+        background-color: var(--ww-color-theme-dark-50);
+        border: 1px solid var(--ww-color-theme-dark-100);
         padding: var(--ww-spacing-01) var(--ww-spacing-02);
-        border-radius: var(--ww-border-radius-01);
+        border-radius: var(--ww-border-radius-02);
     }
 }
 .m-left {
