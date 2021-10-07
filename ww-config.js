18bb4de631020f4b913ba6b03f91ce3b6388077c
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2021-10-07 22:11:59 +0200

    DS for staging

--- ww-config.js
+++ ww-config.js
@@ -1,5 +1,8 @@
 export default {
     editor: {
-        designSystemId: '40cf1c33-2c6e-4d37-b54d-7c877872e235',
+        // PROD
+        // designSystemId: '40cf1c33-2c6e-4d37-b54d-7c877872e235',
+        // STAGING
+        designSystemId: '0f1370f6-8d7b-4bc5-8119-17af840b7468',
     },
 };
