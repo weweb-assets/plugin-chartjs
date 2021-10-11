3c289874016991da60c80dafc52023f377147058
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2021-10-11 10:20:43 +0200

    update DS id

--- ww-config.js
+++ ww-config.js
@@ -3,6 +3,6 @@ export default {
         // PROD
         // designSystemId: '40cf1c33-2c6e-4d37-b54d-7c877872e235',
         // STAGING
-        designSystemId: '0f1370f6-8d7b-4bc5-8119-17af840b7468',
+        designSystemId: '40cf1c33-2c6e-4d37-b54d-7c877872e235',
     },
 };
