1400db6e4fbb198d52a6617c53283631fa762655
Léo Coletta <mail.leo.coletta@gmail.com>
2025-01-14 22:18:21 +0100

    feat: add copilot configuration

--- ww-config.js
+++ ww-config.js
@@ -4,5 +4,20 @@ export default {
         // designSystemId: '40cf1c33-2c6e-4d37-b54d-7c877872e235',
         // STAGING
         designSystemId: '40cf1c33-2c6e-4d37-b54d-7c877872e235',
+        /* wwEditor:start */
+        settings: {
+            copilot: {
+                description: 'Configure Chart.js plugin settings',
+                returns: 'void',
+                schema: {
+                    designSystemId: {
+                        type: 'string',
+                        description: 'The design system ID to use for the Chart.js plugin',
+                        bindable: false
+                    }
+                }
+            }
+        }
+        /* wwEditor:end */
     },
 };
